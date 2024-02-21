1b117f359741e53c266d2aeb3e4ee4d9fd3f3018
yemilRios <sigifredoyemil14@gmail.com>
2024-02-21 10:03:37 -0500

    link scroll menu

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 // ------------------------- menu ----------------------- //
-
+/* stiki nav bar */
 const ul = document.getElementById('ul')
 const hamburger = document.getElementById('ham')
 
 hamburger.addEventListener('click', ()=>{
     ul.classList.toggle('active')
+})
+
+/* active link scrolling */
+
+const navLinks = document.querySelectorAll('.nav_link')
+const sections = document.querySelectorAll('section')
+
+let currentSection = 'home'
+
+window.addEventListener('scroll', ()=>{
+    sections.forEach(section =>{
+        if(window.scrollY >= section.offsetTop){
+            currentSection = section.id
+        }
+    })
+
+    navLinks.forEach(nav_link =>{
+        if(nav_link.href.includes(currentSection)){
+            document.querySelector('.active').classList.remove('active')
+            nav_link.classList.add('active')
+        }
+    })
 })
